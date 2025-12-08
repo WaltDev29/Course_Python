08ef2a57cc76a2bc8324a51731cd638c5e4866ee
Waltdev29 <waltdev29@gmail.com>
2025-12-08 19:35:01 +0900

    계산기 완성

--- term_project/calculator.py
+++ term_project/calculator.py
@@ -4,14 +4,14 @@ window = Tk()
 window.title("KOPO 계산기, 2501110203, 김호석")
 
 window.resizable(width=FALSE, height=FALSE)
-
+window.config(bg="#1E1E1E")
 
 
 # ============ 프레임 설정 ============
-frame_top = Frame(window, bd=1, relief="solid")
-frame_bottom = Frame(window, padx=10, pady=10)
+frame_top = Frame(window, bg="#1E1E1E", highlightbackground="#FFFFFF", highlightthickness=2, relief="solid")
+frame_bottom = Frame(window, bg="#1E1E1E", padx=15, pady=15)
 
-frame_top.pack(side=TOP, fill=BOTH, padx=10, pady=(10,0))
+frame_top.pack(side=TOP, fill=BOTH, padx=15, pady=(15,0))
 frame_bottom.pack(side=BOTTOM, fill=BOTH)
 
 
@@ -21,13 +21,14 @@ frame_bottom.pack(side=BOTTOM, fill=BOTH)
 txt_result, txt_progress = "", ""
 num = ""
 symbol = ""
-result = ""
 
 # 함수
 def put_txt(lbl):
 	global txt_result, txt_progress, num, symbol
 
 	if txt_result == "" and lbl == '.': return
+
+	if txt_progress == "": lbl_progress.config(text="")
 
 	# 숫자 입력 시 txt 업데이트
 	if lbl in map(str, range(0,10)) or lbl == '.':
@@ -42,7 +43,9 @@ def put_txt(lbl):
 				txt_progress = txt_progress[:-2] + lbl + " "
 				lbl_progress.config(text=txt_progress)
 				symbol = lbl
-			else: return
+			else: 
+				lbl_result.config(text="")
+				return
 
 		# num 비어있으면 num에 대입
 		elif num == "":
@@ -87,21 +90,31 @@ def put_txt(lbl):
 # ============ 지우기 함수 ============
 def erase():
 	global txt_result
-	if txt_result == "": return
+	if txt_result == "": 
+		lbl_result.config(text="")
+		lbl_progress.config(text="")
+		return
 	else:
 		txt_result = txt_result[:-1]
 		lbl_result.config(text=txt_result)
-		
+
+def clear():
+	global txt_progress, txt_result, num, symbol
+	txt_progress, txt_result, num, symbol = "", "", "", ""
+	lbl_progress.config(text="")
+	lbl_result.config(text="")
 
 
 # ============ 결과 출력 함수 ============
 def calc_result():
 	global txt_result, txt_progress, num, symbol
 
+	if num == "": return
+
 	# 계산할 두 번째 숫자를 입력하지 않은 경우
 	if txt_result == "":
 		txt_result = str(num)
-		lbl_result.config(text=num)
+		lbl_result.config(text=txt_result)
 		lbl_progress.config(text="")
 
 		# 변수 초기화
@@ -114,7 +127,7 @@ def calc_result():
 		if '.' in txt_result: num2 = float(txt_result)
 		else: num2 = int(txt_result)
 
-		txt_progress = f"{str(num)} {symbol} {str(num2)} = "
+		txt_progress = f"{str(round(num,3))} {symbol} {str(num2)} = "
 
 		# 계산
 		if symbol == '＋': num = num + num2
@@ -131,14 +144,14 @@ def calc_result():
 		symbol = ""
 		txt_result = ""
 		txt_progress = ""
-		# num = ""
+		num = ""
 
 
 
 # ============ 디스플레이 설정 ============
 
-lbl_progress = Label(frame_top, text="", anchor=E)
-lbl_result = Label(frame_top, text="", anchor=E, font=(20))
+lbl_progress = Label(frame_top, text="", bg="#2D2D2D", fg="#FFFFFF", anchor=E, font=("",20))
+lbl_result = Label(frame_top, text="", bg="#2D2D2D", fg="#FFFFFF", anchor=E, font=("",40))
 
 lbl_progress.pack(fill=BOTH)
 lbl_result.pack(fill=BOTH)
@@ -156,27 +169,30 @@ btn_lbls = [
 # 배열 순회하여 버튼 생성 및 배치
 for row, lbls in enumerate(btn_lbls):
 	for col, lbl in enumerate(lbls):
-		btn = Button(frame_bottom, text=lbl, width=5, height=2, command=lambda x=lbl: put_txt(x))
-		btn.grid(row=row, column=col, padx=2, pady=2)
+		color = "#FBC02D" if col == 3 else "#3C3C3C"
+		btn = Button(frame_bottom, text=lbl, width=8, height=3, font=("", 15, "bold"), bg=color, fg="#FFFFFF", command=lambda x=lbl: put_txt(x))
+		btn.grid(row=row, column=col, padx=4, pady=4)
 
 
 # 0, '.', '+' 버튼 생성
-btn_0 = Button(frame_bottom, text='0', width=5, height=2, command=lambda: put_txt('0'))
-btn_dot = Button(frame_bottom, text='.', width=5, height=2, command=lambda: put_txt('.'))
-btn_plus = Button(frame_bottom, text="＋", width=5, height=2, command=lambda: put_txt("＋"))
+btn_0 = Button(frame_bottom, text='0', width=8, height=3, font=("", 15, "bold"), bg="#3C3C3C", fg="#FFFFFF", command=lambda: put_txt('0'))
+btn_dot = Button(frame_bottom, text='.', width=8, height=3, font=("", 15, "bold"), bg="#3C3C3C", fg="#FFFFFF", command=lambda: put_txt('.'))
+btn_plus = Button(frame_bottom, text="＋", width=8, height=3, font=("", 15, "bold"), bg="#FBC02D", fg="#FFFFFF", command=lambda: put_txt("＋"))
 
 # 0, '.', '+' 버튼 배치
-btn_0.grid(row=3, column=0, columnspan=2, sticky=EW, padx=2, pady=2)
-btn_dot.grid(row=3, column=2, padx=2, pady=2)
-btn_plus.grid(row=3, column=3, padx=2, pady=2)
+btn_0.grid(row=3, column=0, columnspan=2, sticky=EW, padx=4, pady=4)
+btn_dot.grid(row=3, column=2, padx=4, pady=4)
+btn_plus.grid(row=3, column=3, padx=4, pady=4)
 
-# '<-', '=' 버튼 생성
-btn_erase = Button(frame_bottom, text="<-", width=5, height=2, command=erase)
-btn_equal = Button(frame_bottom, text="＝", width=5, height=2, command=calc_result)
+# 'C', '←', '=' 버튼 생성
+btn_clear = Button(frame_bottom, text="C",  width=8, height=3, font=("", 15, "bold"), bg="#FF2600", fg="#FFFFFF", command=clear)
+btn_erase = Button(frame_bottom, text="←", width=8, height=3, font=("", 15, "bold"), bg="#FF7300", fg="#FFFFFF", command=erase)
+btn_equal = Button(frame_bottom, text="＝", width=8, height=3, font=("", 15, "bold"), bg="#4FC3F7", fg="#FFFFFF", command=calc_result)
 
-# '<-', '=' 버튼 배치
-btn_equal.grid(row=4, column=0, columnspan=2, sticky=EW, padx=2, pady=2)
-btn_erase.grid(row=4, column=2, columnspan=2, sticky=EW, padx=2, pady=2)
+# 'C', '←', '=' 버튼 배치
+btn_clear.grid(row=4, column=0, sticky=EW, padx=4, pady=4)
+btn_erase.grid(row=4, column=1, sticky=EW, padx=4, pady=4)
+btn_equal.grid(row=4, column=2, columnspan=2, sticky=EW, padx=4, pady=4)
 	
 
 
